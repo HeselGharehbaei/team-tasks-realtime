258df300c0ed5d82d9e1d5e52b155fc6c6671409
hesel <hgharehbaai@gmail.com>
2025-11-02 20:50:37 +0330

    chore: initial project setup with Django, apps, requirements and gitignore

---
+++ team_tasks_realtime/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for team_tasks_realtime project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_tasks_realtime.settings')
+
+application = get_asgi_application()
