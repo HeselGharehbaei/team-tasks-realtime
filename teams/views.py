fb08e30234aa50ecd2f861e2be856fb3c6f2675c
hesel <hgharehbaai@gmail.com>
2025-11-03 23:35:32 +0330

    docs(teams): add documentation for Team model and create-team API

--- teams/views.py
+++ teams/views.py
@@ -1,13 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from drf_spectacular.utils import extend_schema, OpenApiExample
 from .serializers import TeamSerializer
 
 
 class TeamCreateView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-
+    @extend_schema(
+        request=TeamSerializer,
+        responses={201: TeamSerializer},
+        summary="ایجاد تیم جدید",
+        description="کاربر می‌تواند تیم جدید بسازد و اعضا را با ID مشخص کند.",
+        tags=['Teams'],
+        examples=[
+            OpenApiExample(
+                "نمونه درخواست ایجاد تیم",
+                value={
+                    "name": "تیم توسعه",
+                    "members":["2", "3"]
+                },
+                request_only=True
+            ),
+            OpenApiExample(
+                "نمونه پاسخ ایجاد تیم",
+                value={
+                    "id": 1,
+                    "name": "تیم توسعه",
+                    "members_info": [
+                        {"id": 1, "username": "ali"},
+                        {"id": 2, "username": "sara"}
+                    ]
+                },
+                response_only=True
+            ),
+        ]
+    )
     def post(self, request):
         serializer = TeamSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
