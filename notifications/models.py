a1b7f267865d8d2e14fe1604e75c5ab17ee2fd4e
hesel <hgharehbaai@gmail.com>
2025-11-03 12:18:18 +0330

    feat: add Team, Task and Notification models

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+NOTIFICATION_TYPES = [
+        ('assignment', 'Assignment'),
+        ('mention', 'Mention'),
+        ('overdue', 'Overdue'),
+]
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='notifications',
+        on_delete=models.CASCADE
+    )    
+    type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
+    title = models.CharField(max_length=200)
+    message = models.TextField()
+    task = models.ForeignKey(
+        'tasks.Task',
+        related_name='notifications',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user']),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.type} - {self.title}"
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from teams.models import Team
 
-# Create your models here.
+
+PRIORITY_CHOICES = [
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+]
+
+
+class Task(models.Model):
+    title= models.CharField(max_length=250)
+    creator=models.ForeignKey(User, related_name='created_tasks', on_delete=models.CASCADE)
+    description= models.TextField(max_length=500)
+    due_date= models.DateTimeField()
+    priority= models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    assignee=models.ForeignKey(User, null= True, blank= True, related_name='assigned_tasks', on_delete=models.SET_NULL)
+    tagged_users= models.ManyToManyField(User, related_name='tagged_in_tasks')
+    team= models.ForeignKey(Team, related_name='tasks', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+
+     
